2c0fdddfe0f5fe3792e028d2064708cfa4a1cce2
Selena Vargas <smvargas@fullsail.edu>
2016-08-03 00:01:58 -0400

    added single app with some minor changes

--- routes/api.js
+++ routes/api.js
@@ -14,12 +14,12 @@
 
     router.get('/apps', function(req,res){
       res.json({
-        id: "32818-001",
-        title: "Trainer's Gym",
-        description: "An API that can be used to operate inside of a Pokemon gym for users.",
-        releaseDate: "2017-010-13T22:12:20.000ZUTC",
-        createdAt: "s2016-05-13T22:12:20.000ZUTC",
-        updatedAt: "2016-008-13T22:12:20.000ZUTC",
+        id1: "32818-001",
+        title1: "Trainer's Gym",
+        description1: "An API that can be used to operate inside of a Pokemon gym for users.",
+        releaseDate1: "2017-010-13T22:12:20.000ZUTC",
+        createdAt1: "s2016-05-13T22:12:20.000ZUTC",
+        updatedAt1: "2016-008-13T22:12:20.000ZUTC",
 
         id2: "330571-002",
         title2: "Battle Field",
@@ -39,7 +39,7 @@
 
       router.get('/users', function(req,res){
         res.json({
-          user: {
+          user1: {
           id: "025",
           name: "Pikachu",
           Type: "Electric",
@@ -56,6 +56,32 @@
       name: "Squirtle",
       Type: "Water",
     },
+
+    //----------------------- Solo App ---------------------//
+
+    router.get('/apps/:id', function(req,res){
+      res.json({
+        id3: "63131-003",
+        title3: "Pokedex",
+        description3: "An app created to share and engage with others of comic book characters.",
+        releaseDate3: "2018-08-05T22:12:00.000ZUTC",
+        createdAt3: "2018-08-05T22:12:00.000ZUTC",
+        updatedAt3: "2018-08-05T22:12:00.000ZUTC",
+
+  });
+
+    });
+
+
+
+
+
+
+
+
+
+
+
 
       });
         });
