5dcb4424c17b299555784b05e2a8762f26bd4bae
Selena Vargas <smvargas@fullsail.edu>
2016-08-02 23:30:55 -0400

    2nd app added

--- routes/api.js
+++ routes/api.js
@@ -30,4 +30,24 @@
         dob: "1995-10-30:7:59.000ZUTC",
         nationality: "Puerto Rican"
       },
+
+      id2: "33051-002",
+      title2: "Battle Field",
+      description2: "An API that can be used to operate inside of a Pokemon field for users.",
+      artAssests2: [
+        { title: "Brock", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg"},
+        { title: "Meow", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg"},
+      ],
+      releaseDate2: "2016-05-13T22:12:20.000ZUTC",
+      createdAt2: "s2015-05-13T22:12:20.000ZUTC",
+      updatedAt2: "2016-04-13T22:12:20.000ZUTC",
+      user2: {
+        id: "96002",
+        name: "Rashal Ashy",
+        age: "21",
+        dob: "1995-07-27:7:59.000ZUTC",
+        nationality: "Lebanonese"
+      },
+      
+
   }
