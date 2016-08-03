2e0703ca84b5b71c64bc126dfadde4e1180d47bd
Selena Vargas <smvargas@fullsail.edu>
2016-08-02 23:08:33 -0400

    finishing up server

--- src/server.js
+++ src/server.js
@@ -6,3 +6,17 @@ var body_parse = require('body-parser');
 
 // Web Sever
 var app = express();
+
+// Sets up project port
+var port = 3000;
+
+// URL Schema
+app.use('/api/v1', require('../routes/api.js')(express));
+
+// Starts Server
+var server = app.listen(port, function(){
+    console.log('Server Running On:', port);
+  });
+
+  // Makes the server accesible
+  module.exports = server;
