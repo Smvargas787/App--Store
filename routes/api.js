90026667bf6a3e33cc9e7453f767b3e82f237c05
Selena Vargas <smvargas@fullsail.edu>
2016-08-02 23:40:21 -0400

    fixing syntax errors, took a couple steps back

--- routes/api.js
+++ routes/api.js
@@ -11,43 +11,15 @@
     });
 
     //----------------------- All Apps ---------------------//
+    router.get('/users', function(req,res){
+
+    //All Apps Info
     router.get('/apps', function(req,res){
-    res.json({
-      id: "32818-001",
-      title: "Trainer's Gym",
-      description: "An API that can be used to operate inside of a Pokemon gym for users.",
-      artAssests: [
-        { title: "Ash", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg"},
-        { title: "Misty", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg"},
-      ],
-      releaseDate: "2017-05-13T22:12:20.000ZUTC",
-      createdAt: "s2016-05-13T22:12:20.000ZUTC",
-      updatedAt: "2016-08-13T22:12:20.000ZUTC",
-      user: {
-        id: "46214557",
-        name: "Selena Vargas",
-        age: "20",
-        dob: "1995-10-30:7:59.000ZUTC",
-        nationality: "Puerto Rican"
-      },
-
-      id2: "33051-002",
-      title2: "Battle Field",
-      description2: "An API that can be used to operate inside of a Pokemon field for users.",
-      artAssests2: [
-        { title: "Brock", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg"},
-        { title: "Meow", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg"},
-      ],
-      releaseDate2: "2016-05-13T22:12:20.000ZUTC",
-      createdAt2: "s2015-05-13T22:12:20.000ZUTC",
-      updatedAt2: "2016-04-13T22:12:20.000ZUTC",
-      user2: {
-        id: "96002",
-        name: "Rashal Ashy",
-        age: "21",
-        dob: "1995-07-27:7:59.000ZUTC",
-        nationality: "Lebanonese"
-      },
-      
-
-  }
+      res.json({
+        id: "32818-001",
+        title: "Trainer's Gym",
+        description: "An API that can be used to operate inside of a Pokemon gym for users.",
+        releaseDate: "2017-010-13T22:12:20.000ZUTC",
+        createdAt: "s2016-05-13T22:12:20.000ZUTC",
+        updatedAt: "2016-008-13T22:12:20.000ZUTC",
+  };
