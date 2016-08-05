de51b0e57c4a131cd55f1e0e53bbaa5a303b5226
Selena Vargas <smvargas@fullsail.edu>
2016-08-04 23:17:19 -0400

    moved routes folder to src folder

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const port = 3000;
 
 // URL Scheme
-app.use('/api/v1', require('../routes/api')(express));
+app.use('/api/v1', require('./routes/api')(express));
 
 // Starts Server
 const server = app.listen(port, () => {
