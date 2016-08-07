8fde2a9c9a3a90156fafbde5ff0b7c89172bff55
Selena Vargas <smvargas@fullsail.edu>
2016-08-07 15:51:42 -0400

    added create one for users crud routes

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,9 +3,6 @@ const user = require('../../models/user');
 module.exports = (express) => {
   const router = express.Router();
 
-      return router;
-};
-
 
 // ----------------------- Users ---------------------//
 
@@ -18,3 +15,16 @@ module.exports = (express) => {
       res.status(200).json(data);
     });
   });
+
+// Create One (.add)
+
+  router.post('/users', (req, res) => {
+    user.create(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  return router;
+};
