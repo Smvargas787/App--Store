a4fac954353fd08760aa299f820581f783e3acbb
Selena Vargas <smvargas@fullsail.edu>
2016-08-07 16:06:27 -0400

    added create one crud for models app.js

--- src/models/app.js
+++ src/models/app.js
@@ -10,8 +10,8 @@ exports.all = (err, success) => {
   db.app.all().then(success).catch(err);
 };
 
-// // Create One (.add)
-//
-// exports.add = (payload, err, success) => {
-//   db.app.add(payload).then(success).catch(err);
-// };
+// Create One (.add)
+
+exports.add = (payload, err, success) => {
+  db.app.add(payload).then(success).catch(err);
+};
