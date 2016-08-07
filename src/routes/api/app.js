5f201908a0c3e51e841db888fe0de8cdd44d3166
Selena Vargas <smvargas@fullsail.edu>
2016-08-07 15:36:35 -0400

    added create for crud

--- src/routes/api/app.js
+++ src/routes/api/app.js
@@ -3,15 +3,39 @@ const app = require('../../models/app');
 module.exports = (express) => {
   const router = express.Router();
 
-// APPS
+  // ------------------------- All Apps ----------------------------//
 
-// Read Apps
+// Read Apps (.all)
 
+  router.get('/apps', (req, res) => {
+    app.findAll((err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
 
+// Create One App (.add)
 
+  router.post('/apps', (req, res) => {
+    app.create(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
 
-
-
+// // Read One (.one)
+//
+//   router.get('/apps/:id', (req, res) => {
+//     req.body.id = req.params.id;
+//     app.find(req.body, (err) => {
+//       res.status(500).json(err);
+//     }, (data) => {
+//       res.status(200).json(data);
+//     });
+//   });
+//
 
 
   return router;
