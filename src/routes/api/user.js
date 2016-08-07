1a69a5c9e57fdc8ce0da69492fc3a3d066c17622
Selena Vargas <smvargas@fullsail.edu>
2016-08-07 15:47:59 -0400

    added read all for users crud routes

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,3 +1,20 @@
 const user = require('../../models/user');
 
+module.exports = (express) => {
+  const router = express.Router();
+
+      return router;
+};
+
+
 // ----------------------- Users ---------------------//
+
+// Read All (.all)
+
+  router.get('/users', (req, res) => {
+    user.findAll((err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
