f32cf3bf6f94e96ef65f4d9706f9b0718f41e956
Selena Vargas <smvargas@fullsail.edu>
2016-08-07 00:39:52 -0400

    setting up database

---
+++ src/models/db.js
@@ -0,0 +1,46 @@
+  const Sequelize = require('sequelize');
+
+  require('dotenv').config();
+
+  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_SCHEMA,
+    port: process.env.DB_PORT,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+    logging: false,
+  });
+
+  // const user = sequelize.define('user', {
+  //   id: {
+  //     type: Sequelize.STRING,
+  //   },
+  //   name: {
+  //     type: Sequelize.INTEGER,
+  //   },
+  //   type: {
+  //     type: Sequelize.STRING,
+  //   },
+  // });
+  //
+  // const app = sequelize.define('app', {
+  //   name: {
+  //     type: Sequelize.STRING,
+  //   },
+  //   code: {
+  //     type: Sequelize.STRING,
+  //   },
+  // });
+  //
+  // user.hasMany(app, {
+  //   foreignKey: 'userID',
+  // });
+  //
+  // sequelize.sync();
+  //
+  // exports.sequelize = sequelize;
+  // exports.user = user;
+  // exports.app = app;
