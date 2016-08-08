232f8824b77b9bc0ead34f98f82f2d49e246bf28
Selena Vargas <smvargas@fullsail.edu>
2016-08-07 23:26:01 -0400

    fixing all eslint errors

--- src/models/user.js
+++ src/models/user.js
@@ -5,49 +5,49 @@ const db = require('./db');
 
   // Read All (.all)
 
-  exports.all = (err, success) => {
-    db.user.findAll().then(success).catch(err);
-  };
+exports.all = (err, success) => {
+  db.user.findAll().then(success).catch(err);
+};
 
   // Create One (.add)
 
-  exports.add = (payload, err, success) => {
-    db.user.create(payload).then(success).catch(err);
-  };
+exports.add = (payload, err, success) => {
+  db.user.create(payload).then(success).catch(err);
+};
 
   // Read One (.one)
 
-  exports.one = (payload, err, success) => {
-    db.user.find({
-      where: {
-        id: payload.id,
-      },
+exports.one = (payload, err, success) => {
+  db.user.find({
+    where: {
+      id: payload.id,
+    },
         // Defined by sequelize
-      include: [{
-        all: true,
-        nested: true,
-      }],
-    }).then(success).catch(err);
-  };
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
+};
 
   // Update One (.update)
 
-  exports.update = (payload, err, success) => {
-    db.user.find({
-      where: {
-        id: payload.id,
-      },
-    }).then((existingData) => {
-      existingData.updateAttributes(payload).then(success).catch(err);
-    }).catch(err);
-  };
+exports.update = (payload, err, success) => {
+  db.user.find({
+    where: {
+      id: payload.id,
+    },
+  }).then((existingData) => {
+    existingData.updateAttributes(payload).then(success).catch(err);
+  }).catch(err);
+};
 
   // Delete One (.remove)
 
-  exports.remove = (payload, err, success) => {
-    db.user.destroy({
-      where: {
-        id: payload.id,
-      },
-    }).then(success).catch(err);
-  };
+exports.remove = (payload, err, success) => {
+  db.user.destroy({
+    where: {
+      id: payload.id,
+    },
+  }).then(success).catch(err);
+};
