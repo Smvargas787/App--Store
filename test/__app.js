a1398ba1d64af5e1da84a0f4e5b2374dd717c6cf
Selena Vargas <smvargas@fullsail.edu>
2016-08-13 22:46:25 -0400

    setting up app.js test file2

--- test/__app.js
+++ test/__app.js
@@ -1,3 +1,14 @@
 const expect = require('chai').expect;
 const app = require('../src/models/app');
 const util = require('../lib/util');
+
+let testApp = {};
+
+// -------------------------- Tests Set Up ----------------------------//
+  describe('Apps', () => {
+    beforeEach((done) => {
+      const mockApp = {
+        title: 'WheresPikachu?',
+        description: 'Reverse game of wheres waldo',
+        releaseDate: '2016-12-01'
+      };
