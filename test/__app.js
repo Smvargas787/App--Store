04051039020d45892eb32d255132411862916128
Selena Vargas <smvargas@fullsail.edu>
2016-08-13 23:02:34 -0400

    Creating update for app test

--- test/__app.js
+++ test/__app.js
@@ -5,15 +5,15 @@ const util = require('../lib/util');
 let testApp = {};
 
   // -------------------------- Tests Set Up ----------------------------//
-  describe('Apps', () => {
-    beforeEach((done) => {
-      const mockApp = {
-        title: 'WheresPikachu?',
-        description: 'Reverse game of wheres waldo',
-        releaseDate: '2016-12-01'
-      };
+    describe('Apps', () => {
+      beforeEach((done) => {
+        const mockApp = {
+          title: 'WheresPikachu?',
+          description: 'Reverse game of wheres waldo',
+          releaseDate: '2016-12-01'
+        };
 
-    // -------------------------- Creating Mocha Stub ----------------------------//
+  // -------------------------- Creating Mocha Stub ----------------------------//
       app.create(mockApp, (error) => {
         util.debug('Error! Pikachus cannot be man created.', error);
       }, (newDBapp) => {
@@ -22,7 +22,7 @@ let testApp = {};
       });
     });
 
-    // -------------------------- Mutiple Apps DB Stopper ----------------------------//
+  // -------------------------- Mutiple Apps DB Stopper ----------------------------//
       afterEach((done) => {
         app.destroy(testApp, (error) => {
           util.debug('Not Possible to Delete A Pikachu', error);
@@ -32,7 +32,7 @@ let testApp = {};
         });
       });
 
-      // -------------------------- Reading Tests ----------------------------//
+  // -------------------------- Reading Tests ----------------------------//
         it('Read All', (done) => {
           app.findAll((error) => {
             util.debug('Failed to find all the pikachus in Apps', error);
@@ -42,17 +42,26 @@ let testApp = {};
           });
         });
 
-        // -------------------------- 1 App Test Reader ----------------------------//
-            it('Read One', (done) => {
-              app.find(testApp, (error) => {
-                util.debug('Cannot find One Pikachu in this App anywhere', error);
-              }, (oneApp) => {
-                expect(oneApp.id).to.be.equal(testApp.id);
-                done();
-              });
-            });
-
-        // -------------------------- 1 App Test Creater ----------------------------//
-          it('Create One', () => {
-            expect(testApp.id).to.not.be.null;
+  // -------------------------- 1 App Test Reader ----------------------------//
+        it('Read One', (done) => {
+          app.find(testApp, (error) => {
+            util.debug('Cannot find One Pikachu in this App anywhere', error);
+          }, (oneApp) => {
+            expect(oneApp.id).to.be.equal(testApp.id);
+            done();
           });
+        });
+
+  // -------------------------- 1 App Test Creater ----------------------------//
+        it('Create One', () => {
+          expect(testApp.id).to.not.be.null;
+        });
+
+  // -------------------------- 1 App Test Updater ----------------------------//
+        it('Update One', (done) => {
+          const updateApp = {
+            id: testApp.id,
+            title: 'Poke Hide & Seek',
+            description: 'An ispy of random sightings of pokemon around the globe',
+            releaseDate: '2016-10-30'
+          };
