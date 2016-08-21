c8eacb13254a077535eae57a08800a749e255655
Selena Vargas <smvargas@fullsail.edu>
2016-08-20 23:14:58 -0400

    adding load  to dotenv file in db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,7 @@
   const Sequelize = require('sequelize');
 
   // ENV File Loaded
-  require('dotenv').config({ silent: true });
+  require('dotenv').load();
 
   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
