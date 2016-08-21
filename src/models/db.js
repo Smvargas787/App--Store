3874b98b1f02c440cfaeb8eabe5a5d5122f8a6fb
Selena Vargas <smvargas@fullsail.edu>
2016-08-20 23:06:59 -0400

    adding silence to dotenv file in db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,7 @@
   const Sequelize = require('sequelize');
 
   // ENV File Loaded
-  require('dotenv').config();
+  require('dotenv').config({ silent: true });
 
   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
