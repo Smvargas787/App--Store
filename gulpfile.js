d12e88fade2ad109c4f2a60f6db8246cd4f8ec64
Selena Vargas <smvargas@fullsail.edu>
2016-08-25 16:03:07 -0400

    setting up Git Add

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ const gulp = require('gulp');
 const git = require('gulp-git');
 const args = require('yargs');
 
+
+// -------------------------- Tasks Set Up ----------------------------//
 gulp.task('increase', () => {
   const packageJson = require('./package.json');
 
   console.log(packageJson.version);
+});
+
+// -------------------------- Git Add ----------------------------//
+gulp.task('add', () => {
+  console.log('Adding New Version: \n');
+  return gulp.src('./git-test/*')
+    .pipe(git.add({ args: ' -A' }));
 });
