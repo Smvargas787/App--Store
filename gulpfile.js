a9116a911ea631eb4ebb695c5fed4f1ddd2984a1
Selena Vargas <smvargas@fullsail.edu>
2016-08-25 23:38:29 -0400

    adding in versions m,m,p

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,76 @@
 const gulp = require('gulp');
 const git = require('gulp-git');
-const args = require('yargs');
+const argv = require('yargs').argv;
+const packageJSON = require('./package.json');
+const runSequence = require('run-sequence');
+const bump = require('gulp-bump');
 
+let version = packageJSON.version;
+
+exports.vni = (vn, vt) => {
+  const file = '/tmp/data.json';
 
 // -------------------------- Tasks Set Up ----------------------------//
 
-gulp.task('bump', () => {
-  const packageJson = require('./package.json');
-
-  console.log(packageJson.version);
-});
-
-// -------------------------- Git Add ----------------------------//
-
-gulp.task('add', () => {
-  console.log('Adding New Version: \n');
-  return gulp.src('./git-test/*')
-    .pipe(git.add({ args: ' -A' }));
-});
-
-// -------------------------- Git Commit/Tag ----------------------------//
-
-gulp.task('tag', ['commit'], () => {
-  console.log('Commiting & Tagging Version: \n');
-  git.tag('v1.8.0', 'Version Message', (err) => {
-    if (err) throw err;
+  gulp.task('default', () => {
+    runSequence('tag', 'pushTag');
   });
-});
 
-// -------------------------- Git Push ----------------------------//
+// -------------------------- Initializing Major, Minor, Patch Versions ----------------------------//
 
-gulp.task('push', () => {
-  console.log('Pushing New Version: \n');
-  git.push('github', 'gulptask', (err) => {
-    if (err) throw err;
+  gulp.task('tag', () => {
+    let major = [0];
+    let minor = [1];
+    let patch = [2];
+    const packageBump = {};
+
+    const versionArray = [];
+    for (let i = 0; i < version.length; i++) {
+      if (version[i] === '.') {
+        versionArray.push(i);
+      }
+    }
+    patch = vt.toLowerCase(versionArray[2] + 2, vt.length);
+    minor = vt.toLowerCase(versionArray[0] + 1, versionArray[1]);
+    major = vt.toLowerCase(versionArray[0], versionArray[0] - vt.length);
+
+    if (argv.major && argv.minor || argv.minor && argv.patch || argv.patch && argv.major) {
+      console.warn('Accepting Only Major, Minor, or Patch.');
+    } else {
+      if (argv.major) {
+        major++;
+        minor = 0;
+        patch = 0;
+      } else if (argv.minor) {
+        minor++;
+      } else if (argv.patch) {
+        patch++;
+      } else {
+        console.log('Update: None');
+        git.checkout('bump', (err) => {
+          if (err) throw err;
+        });
+        process.exit();
+      }
+    }
+    version = `${major}.${minor}.${patch}`;
+    packageBump.version = version;
+
+  // -------------------------- Git Tag ----------------------------//
+
+    git.tag('v' + version, argv.message, { args: '-a' }, (err) => {
+      if (err) throw err;
+    });
+    gulp.src('./*.json')
+    .pipe(bump({ version: packageBump.version }))
+    .pipe(gulp.dest('./'));
   });
-});
 
-// -------------------------- Git Push & Tagging ----------------------------//
+// -------------------------- Git Push-Tag ----------------------------//
 
-gulp.task('push-tag', ['push'], () => {
-  console.log('Pushing-Tagged Version: \n');
-  git.push('github', 'gulptask', { args: ' --tags' }, (err) => {
-    if (err) throw err;
+  gulp.task('pushTag', () => {
+    git.push('github', [], { args: '--tags' }, (err) => {
+      if (err) throw err;
+    });
   });
-});
-
-// -------------------------- Git Pushed Finalized ----------------------------//
-
-gulp.task('default', ['add', 'commit', 'tag', 'push', 'push-tag'], () => {
-  console.log('Push Test Sucessful');
-});
+};
